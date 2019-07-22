57f97b96516ff82e452cc2205d94f1c77b4d355d
risa <az.5s.fmt@gmail.com>
2019-07-22 16:45:26 +0900

    systemテスト/books_testの追加

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -4,44 +4,41 @@ require 'application_system_test_case'
 
 class BooksTest < ApplicationSystemTestCase
   setup do
-    @book = books(:one)
+    @book = books(:book1)
+    @user = users(:user1)
+    login_as(@user)
   end
 
   test 'visiting the index' do
-    visit books_url
-    assert_selector 'h1', text: 'Books'
+    visit books_path
+    assert_selector 'h1', text: '図書'
   end
 
   test 'creating a Book' do
-    visit books_url
-    click_on 'New Book'
-
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Create Book'
-
-    assert_text 'Book was successfully created'
-    click_on 'Back'
+    visit books_path
+    click_on '新規作成'
+    fill_in 'タイトル', with: @book.title
+    fill_in 'メモ', with: @book.memo
+    click_on '登録する'
+    assert_text '書籍を追加しました'
+    click_on '戻る'
   end
 
   test 'updating a Book' do
-    visit books_url
-    click_on 'Edit', match: :first
-
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Update Book'
-
-    assert_text 'Book was successfully updated'
-    click_on 'Back'
+    visit books_path
+    click_on '編集'
+    fill_in 'メモ', with: @book.memo
+    fill_in 'タイトル', with: @book.title
+    click_on '更新する'
+    assert_text '書籍を更新しました'
+    click_on '戻る'
   end
 
   test 'destroying a Book' do
-    visit books_url
+    visit books_path
     page.accept_confirm do
-      click_on 'Destroy', match: :first
+      click_on '削除'
     end
-
-    assert_text 'Book was successfully destroyed'
+    assert_text '書籍を削除しました'
   end
 end
