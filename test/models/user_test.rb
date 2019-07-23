8c58ec6188435149fe612dba474e9993f81e510e
risa <az.5s.fmt@gmail.com>
2019-07-23 21:47:39 +0900

    model/systemテストのRubocop修正

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,28 +3,26 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
   setup do
     @user1 = users(:user1)
   end
 
-  test "see if auth and user have the same account" do
-    user = User.new(provider: "github", uid: "01")
+  test 'see if auth and user have the same account' do
+    user = User.new(provider: 'github', uid: '01')
     omniauth = User.from_omniauth(user)
     assert_equal @user1, omniauth
   end
 
-  test "save a new account if auth and user do not have the same account" do
-    user = OmniAuth::AuthHash.new({
+  test 'save a new account if auth and user do not have the same account' do
+    user = OmniAuth::AuthHash.new(
       provider: 'github',
       uid: '02',
       info: {
         email: 'user02@exmaple.com'
       },
       password: Devise.friendly_token[0, 20]
-    })
+    )
     omniauth = User.from_omniauth(user)
-    assert_equal omniauth, User.where(provider: "github", uid: "02").first
+    assert_equal omniauth, User.where(provider: 'github', uid: '02').first
   end
-
 end
--- test/system/registrations_test.rb
+++ test/system/registrations_test.rb
@@ -1,4 +1,6 @@
-require "application_system_test_case"
+# frozen_string_literal: true
+
+require 'application_system_test_case'
 
 class RegistrationsTest < ApplicationSystemTestCase
   # test "visiting the index" do
@@ -11,11 +13,11 @@ class RegistrationsTest < ApplicationSystemTestCase
     login_as(@user)
   end
 
-  test "editing a User" do
+  test 'editing a User' do
     visit edit_user_registration_path
     fill_in 'パスワード', with: 'newpassword'
     fill_in 'パスワード（確認用）', with: 'newpassword'
-    fill_in '現在のパスワード', with: "password"
+    fill_in '現在のパスワード', with: 'password'
     click_on 'Update'
     assert_text 'アカウント情報を変更しました'
   end
